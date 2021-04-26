56b7757cf0f4029f1380f213651ddabb2751273c
FriskyChuks <chuksedwin.ce@gmail.com>
2021-04-26 16:10:27 +0100

    db remodified for mysql

--- emr/settings/production.py
+++ emr/settings/production.py
@@ -113,10 +113,10 @@ DATABASES = {
 }
 
 # Overwrite DB settings to use POSTGRESQL in Heroku
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
