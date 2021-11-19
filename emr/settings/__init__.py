e6ecf997ce39fc7fdac9bfa8ed14f32d5279ff1e
Chuks Edwin <chuksedwin.ce@gmail.com>
2021-11-19 14:35:00 +0100

    pharm module updated

--- emr/settings/__init__.py
+++ emr/settings/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # from .base import * #mysql
 
 from .production import * # Heroku
@@ -7,13 +6,3 @@ from .production import * # Heroku
 #     from .local import * #PG ADMIN 4
 # except:
 #     pass
-=======
-# from .base import * #mysql
-
-from .production import * # Heroku
-
-# try:
-#     from .local import * #PG ADMIN 4
-# except:
-#     pass
->>>>>>> 5727e0ae5d93c7685cd89dc0ba59341e4321373a
