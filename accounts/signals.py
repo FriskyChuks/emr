1c3ccf2f658873e35c14fcf283b274e4fe9d3dcf
unknown <chuksedwin.ce@gmail.com>
2022-09-29 21:48:12 +0100

    accounts signals modified

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from .models import User
 
 
-@receiver(post_save, sender=User)
-def post_save_update_sub_total(sender, instance, created, **kwargs):
-    if created:
-        User.objects.filter(id=instance.id).update(group_id=1)
+# @receiver(post_save, sender=User)
+# def post_save_update_sub_total(sender, instance, created, **kwargs):
+#     if created:
+#         User.objects.filter(id=instance.id).update(group_id=1)
