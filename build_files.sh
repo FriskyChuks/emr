26baab1502f8e1a2dd2e245a3f72ebc775f41db9
MedTech TCRC <chuksedwin.ce@gmail.com>
2023-05-19 13:31:51 +0100

    updated build files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 echo "BUILD START"
 pip install -r requirements.txt
+
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
