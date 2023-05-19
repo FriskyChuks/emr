985ce90a52efb1c290c8812e1fadc038d2c8a844
MedTech TCRC <chuksedwin.ce@gmail.com>
2023-05-19 15:23:46 +0100

    build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 
 echo "Make Migration..."
 # python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py migrate --noinput
 
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
